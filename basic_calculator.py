6edce773493ac7b40a9b8f688486497b34ff5d5f
Mahendra-P-Mainkar <mahendramainkarindia@gmail.com>
2026-07-17 21:43:49 +0530

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,16 @@
+a = 10
+b = 5
+sum = a+b
+print("Sum of",a,"and",b,"is",sum)
+sub = a-b
+print("The subtraction of",a,"and",b,"is",sub)
+pord = a*b
+print("The product of",a,"and",b,"is",pord)
+expo = a**b
+print("The exponent of",a,"and",b,"is",expo)
+divi = a/b
+print("The division of",a,"and",b,"is",divi)
+mod = a%b
+print("The modulus of",a,"and",b,"is",mod)
+fdivi = a//b
+print("The floor division of",a,"and",b,"is",fdivi)
